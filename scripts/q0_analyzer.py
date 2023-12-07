cfa4ea54007ebe8c134434a6ff8cf13101deb76a
Alexandre <gottela@cardiff.ac.uk>
2023-12-07 13:22:43 +0000

    Now able to calculate MC q0 distr. for each candidate.

--- scripts/q0_analyzer.py
+++ scripts/q0_analyzer.py
@@ -1,18 +1,224 @@
 """File to find significant candidates in DM q0 data by comparing MC and data distributions."""
 import os
+import glob
 from multiprocessing import Pool
-from tqdm import trange, tqdm
+import h5py
+from tqdm import tqdm
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.gridspec import GridSpec
 from scipy.stats import norm, skewnorm
 from scipy.optimize import minimize
+from scipy.interpolate import interp1d
+from scipy import constants
 # Project imports
 import hist
 import utils
 import sensutils
 import models
+
+
+def log_likelihood(params, Y, bkg, peak_norm, peak_shape, model_args):
+    """Likelihood of finding dark matter in the data."""
+    # Actual likelihood calculation
+    mu_DM = params
+    try:
+        residuals = Y - np.log(np.exp(bkg) + peak_norm*peak_shape*mu_DM)
+    except Exception as err:
+        raise err
+    log_lkl = sensutils.logpdf_skewnorm(
+        residuals, model_args[..., 0], model_args[..., 1], model_args[..., 2])
+
+    # Add infinity protection - breaks norm but ok if empirical tests
+    mask = np.isinf(log_lkl)
+    if np.sum(mask):
+        row_mins = np.min(np.where(mask, np.inf, log_lkl), axis=1)
+        log_lkl[mask] = np.take(row_mins, np.where(mask)[0])
+
+    return np.sum(np.sum(log_lkl, axis=1))
+
+
+class PeakShape(np.ndarray):
+    """Hold peak shape (varies over freq.)"""
+
+    def __new__(cls, f, path, dtype=float, buffer=None, offset=0, strides=None, order=None):
+        peak_arrays = np.load(path)
+        shape = (peak_arrays["data"].shape[1],)
+        frequency_bounds = peak_arrays["bounds"]
+
+        obj = super(PeakShape, cls).__new__(cls, shape, dtype, buffer, offset, strides, order)
+        obj.f = np.array([f])
+        obj.frequency_bounds = frequency_bounds
+        obj.peak_shapes = peak_arrays["data"]
+        obj._update_array()
+        return obj
+
+    def _update_array(self):
+        """Update the array based on the value of f."""
+        condition = np.where(self.f > self.frequency_bounds)[0]
+        idx = 0 if condition.size == 0 else condition[-1] + 1
+        np.copyto(self, self.peak_shapes[idx, :])
+
+    def update_freq(self, f):
+        """Update the frequency value (and as such the peak shape array if necessary)."""
+        self.f = np.array([f])
+        self._update_array()
+
+
+class DataManager:
+    """Manager LPSD data for candidate search."""
+
+    def __init__(self, data_path=None, json_path=None, tf_dir_path=None,
+                 peak_shape_path=None, data_prefix="result"):
+        """Open HDF files and prepare working at different freqs."""
+        self.rho_local = 0.4 / (constants.hbar / constants.e
+                                * 1e-7 * constants.c)**3  # GeV/cm^3 to GeV^4
+
+        self.peak_shape = PeakShape(0, peak_shape_path)
+        self.len_peak = len(self.peak_shape)
+        self.transfer_functions, self.f_A_star = self.get_f_A_star(tf_dir_path)
+        self.data_info = self.get_info_from_data_path(data_path, json_path,
+                                                      prefix=data_prefix)
+
+    def __del__(self):
+        """Close HDFs properly."""
+        if hasattr(self, "data_info"):
+            for dfile, _, _ in self.data_info:
+                dfile.close()
+
+    def parse_ifo(self, i):
+        """Parse the ifo from the dset attr at index i."""
+        return self.dset.attrs[str(i)].split("_")[-1]
+
+    def get_info_from_data_path(self, data_path, json_path, prefix="result"):
+        """Get all possible info from result files in data_path and json_path."""
+        data_paths = sorted(list(glob.glob(os.path.join(data_path, f"{prefix}*"))))
+        data_info = []
+        for _data_path in tqdm(data_paths, desc="Opening data HDFs", leave=False):
+            _data_info = []
+            _data_info.append(h5py.File(sensutils.get_corrected_path(_data_path), "r"))
+            ifo = sensutils.parse_ifo(_data_path)
+            _data_info.append(ifo)
+            df_key = "splines_" + sensutils.get_df_key(_data_path)
+            _data_info.append(sensutils.get_results(df_key, json_path))
+            assert _data_info[-1] is not None
+            # _data_info.append(df_key)
+            data_info.append(_data_info)
+
+        return data_info
+
+    def get_f_A_star(self, tf_dir):
+        """Get dicts of transfer function info."""
+        transfer_functions = {}
+        transfer_functions["H1"] = pd.read_csv(os.path.join(tf_dir, "Amp_Cal_LHO.txt"),
+                                            delimiter="\t")
+        transfer_functions["L1"] = pd.read_csv(os.path.join(tf_dir, "Amp_Cal_LLO.txt"),
+                                            delimiter="\t")
+        f_A_star = {"H1": interp1d(transfer_functions["H1"]["Freq_o"],
+                                transfer_functions["H1"]["Amp_Cal_LHO"]),
+                    "L1": interp1d(transfer_functions["L1"]["Freq_Cal"],
+                                transfer_functions["L1"]["amp_cal_LLO"])}
+        return transfer_functions, f_A_star
+
+    def simulate_q0_at_freq(self, test_freq, Nsim,
+                            batch_size=10000, n_processes=1):
+        """Simulate q0 assuming mu=0."""
+        # Need bkg, model_args, peak_norm, peak_shape
+        df_columns = ["x_knots", "y_knots", "alpha_skew", "loc_skew", "sigma_skew"]
+        freq_Hz, bkg, beta = [np.zeros((len(self.data_info), self.len_peak))
+                              for _ in range(3)]
+        model_args = np.zeros((len(self.data_info), 3))
+        for i, (hdf_path, ifo, df) in enumerate(self.data_info):  # For each segment
+            # Find the df entry that contains the tested frequency
+            mask = (df['fmin'] <= test_freq) & (df['fmax'] >= test_freq)
+            # Skip this entry if test_freq is out of bounds
+            if np.sum(np.array(mask, dtype=int)) == 0:
+                continue
+            x_knots, y_knots, alpha_skew, loc_skew, sigma_skew\
+                = df[mask][df_columns].iloc[0]
+
+            # Find closest matching frequency
+            frequencies = hdf_path["frequency"]
+            frequency_idx = sensutils.binary_search(frequencies, test_freq)
+
+            # Gather relevant data
+            # logPSD[i, :] = hdf_path["logPSD"][frequency_idx:frequency_idx+self.len_peak]
+            freq_Hz[i, :] = frequencies[frequency_idx:frequency_idx+self.len_peak]
+            model_args[i, :] = [alpha_skew, loc_skew, sigma_skew]
+
+            # Construct background
+            bkg[i, :] = models.model_xy_spline(
+                np.concatenate([x_knots, y_knots]), extrapolate=True)(
+                    np.log(frequencies[frequency_idx:frequency_idx+self.len_peak])
+            )
+
+            # Set transfer function data
+            A_star_sqr = self.f_A_star[ifo](freq_Hz[i, :])**2
+            beta[i, :] =\
+                self.rho_local / (np.pi*freq_Hz[i, :]**3 *
+                                  A_star_sqr * (constants.e / constants.h)**2)
+
+        # Simulate following model args and calculate q0
+        def make_args():
+            endpoints = np.arange(0, Nsim, batch_size)
+            if endpoints[-1] != Nsim:
+                endpoints = np.concatenate([endpoints, [Nsim]])
+            for j, (start, end) in enumerate(zip(endpoints[:-1], endpoints[1:])):
+                yield j, end - start, bkg, beta, model_args, self.peak_shape
+
+        results = []
+        with Pool(n_processes, maxtasksperchild=100) as pool:
+            with tqdm(total=Nsim, desc="Calculate q0") as pbar:
+                for result in pool.imap_unordered(get_q0_batch, make_args()):
+                    results.append(result)
+                    pbar.update(len(result[1]))
+
+        # Merge results
+        q0_data = np.zeros((Nsim, 4))
+        for [i, _q0] in results:
+            q0_data[i:i+_q0.shape[0], :] = _q0
+        return q0_data
+
+
+def get_q0_batch(args):
+    """Calculate N q0 values - formatted for multiprocessing."""
+    i, N, bkg, beta, model_args, peak_shape = args
+    # Generate background
+    alpha, mu, sigma = [np.repeat(model_args[:, j][:, None], N, axis=1) for j in range(3)]
+    MC_bkg = bkg + skewnorm.rvs(alpha, loc=mu, scale=sigma, size=(bkg.shape[0], N))
+
+    # Maximise lkl
+    def log_lkl(params, Y):
+        return log_likelihood(params[0], Y, bkg, beta, peak_shape, model_args)
+
+    # We can assume that most mu values in one batch will be similar
+    # So get the initial guess only once, then use it for all
+    test_mus = np.logspace(-40, -32, 1000)
+    idx, mask = 0, np.zeros(0, dtype=bool)
+    while not any(~mask):
+        test_lkl = np.array([-log_lkl([mu], MC_bkg[:, idx]) for mu in test_mus])
+        mask = np.isnan(test_lkl) | np.isinf(test_lkl)
+        idx += 1
+        if idx == N:
+            print("mh")
+            return i, np.array([np.nan]*N)
+    initial_guess = test_mus[np.argmin(test_lkl[~mask])]
+
+    # To mimic input format
+    q0_data = np.zeros((N, 4))
+    for j in range(N):
+        popt = minimize(lambda x: -log_lkl(x, MC_bkg[:, j]),
+                        [initial_guess], bounds=[(0, None)],
+                        method="Nelder-Mead", tol=1e-10)
+
+        # Get max & zero lkl
+        zero_lkl = log_lkl([0], MC_bkg[:, j])
+        max_lkl = -popt.fun
+        if popt.x > 0:
+            q0_data[j, 1:] = [zero_lkl, max_lkl, popt.x]
+
+    return i, q0_data
 
 
 def extract_clean_q0(q0_data):
@@ -55,7 +261,7 @@ def get_y_spline(x, *params):
     return models.model_xy_spline(params, extrapolate=True)(x)
 
 
-def get_chi_sqr(x, y, y_spline, distr_popt, nbins=75):
+def get_chi_sqr(y, y_spline, distr_popt, nbins=75):
     """Calculate chi^2/dof."""
     h0, bins = np.histogram(y - y_spline, nbins)
     bin_centers = (bins[1:] + bins[:-1]) / 2.
@@ -102,8 +308,8 @@ def get_bic(x, y, n_knots, buffer=20, nbins=100, verbose=False):
     alpha, mu, sigma = sensutils.get_skewnorm_p0(res)
 
     # FIXME
-    if alpha < -20:
-        alpha = -5
+    if np.abs(alpha) > 5:
+        alpha = np.sign(alpha)*5
 
     # 6. Fit a skew normal
     def fitFunc(_x, *params):
@@ -140,7 +346,7 @@ def process_iteration(params):
     plt.savefig(os.path.join(plotdir, f"{prefix}_spline_{suffix}"))
     plt.close()
     y_spline = models.model_xy_spline(f_popt, extrapolate=True)(x)
-    chi_sqr = get_chi_sqr(x, y, y_spline, distr_popt, nbins)
+    chi_sqr = get_chi_sqr(y, y_spline, distr_popt, nbins)
 
     res = y - best_fit
     bins = np.linspace(min(res), max(res), nbins)
@@ -250,6 +456,7 @@ def hybrid_dataloader(q0_path, lbl, verbose=False, pruning=100,
 
 def get_q0_candidates(q0_data_path=None, prefix="", dfdir="endgame",
                       threshold_sigma=5, verbose=False):
+    """Create a list of peaks in q0 based on threshold from whitened data."""
     # Fit info
     df_columns = ["fmin", "fmax", "x_knots", "y_knots",
                   "alpha_skew", "loc_skew", "sigma_skew", "chi_sqr"]
@@ -352,8 +559,8 @@ def clusterize_candidates(idx, freqs, heights, sigmas):
     return _freqs, _heights, _sigmas
 
 
-def main(q0_data_path, prefix, do_hybrid=False, dfdir="endgame",
-         **q0_candidate_kwargs):
+def main(q0_data_path, prefix, DM_kwargs,
+         do_hybrid=False, dfdir="endgame", **q0_candidate_kwargs):
     """Coordinate analysis."""
     # Fit blocks
     if do_hybrid:
@@ -364,6 +571,7 @@ def main(q0_data_path, prefix, do_hybrid=False, dfdir="endgame",
     q0_candidate_kwargs.update({"dfdir": dfdir,
                                 "q0_data_path": q0_data_path,
                                 "prefix": prefix})
+    # TODO: You'll have to think about idx positions if len_peak > 1
     idx, freqs, heights, sigmas = get_q0_candidates(**q0_candidate_kwargs)
     n_candidates = 0
     for freq_group in freqs:
@@ -377,19 +585,34 @@ def main(q0_data_path, prefix, do_hybrid=False, dfdir="endgame",
 
     # For each candidate, simulate raw q0 distribution assuming mu=0
     # (to get significance) and compare with asymptotic scenario
+
+    mngr = DataManager(**DM_kwargs)
+    Nsim = 1 + int(1. / (1 - norm.cdf(5)))  # FIXME LEE-Correction
+    Nsim = Nsim // 10
+    batch_size = 300
     for frequency, height in zip(freqs, heights):
-        # Get data around that peak
-        # Setup q0 calculation
-        # Decide on Nsim
-        # Simulate & hist
-        # Compare with asymptotics
-        pass
+        # Plot candidate data
+        # TODO
+        # Get q0 distribution
+        q0_data = mngr.simulate_q0_at_freq(
+            frequency, Nsim, batch_size=batch_size, n_processes=10)
+        np.save("q0_data_test1.npy", q0_data)
+        # Plot q0 distribution and asymptotic formula
+        q0 = extract_clean_q0(q0_data)
+        ax = hist.plot_hist(np.sqrt(q0), 75, logy=True)
+        ax.axvline(np.sqrt(np.exp(height)), color="r", linestyle="--")
 
 
 
 if __name__ == '__main__':
-    kwargs = {"dfdir": "endgame",
-              "threshold_sigma": 5,
-              "verbose": False}
+    q0_kwargs = {"dfdir": "endgame",
+                 "threshold_sigma": 5,
+                 "verbose": False
+    }
+    DM_kwargs = {"data_path": "data/",
+                 "json_path": "data/processing_results.json",
+                 "peak_shape_path": "single_bin_peak_shape_data.npz",
+                 "tf_dir_path": "data/transfer_functions"
+    }
     # main("singlebin_MC_noise_q0_data.npy", "noise", **kwargs)
-    main("singlebin_data_q0_data.npy", "data", **kwargs)
+    main("singlebin_data_q0_data.npy", "data", DM_kwargs, **q0_kwargs)
