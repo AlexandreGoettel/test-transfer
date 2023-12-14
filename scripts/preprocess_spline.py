d0fe0dc9c989959859800fffe3f234c7bf3808df
Alexandre <gottela@cardiff.ac.uk>
2023-12-14 10:11:56 +0000

    Remove deprecated preprocess code.

--- scripts/preprocess_spline.py
+++ scripts/preprocess_spline.py
@@ -12,9 +12,6 @@ from matplotlib.gridspec import GridSpec
 from scipy.stats import skewnorm
 from scipy.interpolate import CubicSpline
 from scipy.optimize import minimize
-# Custom imports
-import ultranest
-from ultranest.plot import cornerplot
 # Project imports
 from peak_finder import PeakFinder
 import sensutils
@@ -33,96 +30,6 @@ def parse_inputs():
     parser.add_argument("--verbose", action="store_true")
 
     return vars(parser.parse_args())
-
-
-def prior_uniform(cube, initial, sigma, epsilon=.05, n_sigma=3):
-    """Define uniform prior around initial*(1 +- epsilon)."""
-    param = cube[np.newaxis, :].copy() if cube.ndim == 1 else cube.copy()
-    shift = len(sigma)
-
-    # Spline params
-    lo, hi = initial[shift:] * (1. - epsilon), initial[shift:] * (1. + epsilon)
-    param[:, shift:] = lo + param[:, shift:] * (hi - lo)
-
-    # Distribution params
-    lo, hi = initial[:shift] - n_sigma*sigma, initial[:shift] + n_sigma*sigma
-    param[:, :shift] = lo + param[:, :shift] * (hi - lo)
-    return param
-
-
-def model_spline(cube, x_data, x_knots, shift=2):
-    """Vectorize spline model."""
-    output = np.zeros((cube.shape[0], len(x_data)))
-    for i in range(cube.shape[0]):
-        output[i, :] = CubicSpline(x_knots, cube[i, shift:], extrapolate=False)(x_data)
-    return output
-
-
-def likelihood_spline_skew(cube, x, y_data, x_knots, shift=2):
-    """Define skew-normal likelihood function around spline."""
-    y_model = model_spline(cube, x, x_knots, shift=shift)
-    alpha, sigma = cube[:, 0], np.sqrt(np.abs(cube[:, 1]))
-    mode = sensutils.get_mode_skew(alpha, np.zeros_like(alpha), sigma)
-    lkl = skewnorm.logpdf((y_data - y_model).T,
-                          alpha,
-                          scale=sigma,
-                          loc=-mode)
-    return np.sum(lkl, axis=0)
-
-
-def perform_single_fit(x, y, x_knots,
-                       nlive=128, dlogz=.5, pruning=10, verbose=False):
-    """Perform single Bayesian optimisation, return evidence."""
-    # 1. Set initial guess along straight line
-    x, y = x[::pruning], y[::pruning]
-    a, b = np.polyfit(x, y, 1)
-
-    # 2. Find initial parameters for the skew norm shape
-    def fitFunc(x, A, mu, alpha, sigma):
-        return A*skewnorm.pdf(x, alpha, scale=np.sqrt(sigma), loc=mu)
-
-    res = y - CubicSpline(x_knots, a*x_knots+b, extrapolate=False)(x)
-    bins = np.linspace(min(res), max(res), 100)
-    popt, pcov = hist.fit_hist(fitFunc, res, bins, p0=[len(x), 0, 0, 1])
-    skew_uncertainty = np.sqrt(np.diag(pcov)[2:])
-
-    # 3. Find total initial parameters using global minimisation
-    initial_guess = np.concatenate(
-        [[popt[2], popt[3]], a*x_knots + b +
-         sensutils.get_mode_skew(popt[2], popt[1], np.sqrt(popt[3]))])
-    def lkl(params):
-        out = likelihood_spline_skew(params[None, :], x, y, x_knots, shift=2)
-        return -out
-    popt = minimize(lkl, x0=initial_guess, method="Powell")
-    assert popt.success
-
-    # 3. Start Bayesian optimisation procedure around optimal point
-    def prior(cube, *_):
-        return prior_uniform(cube, popt.x, skew_uncertainty,
-                             epsilon=.03, n_sigma=3)
-
-    def likelihood(cube, *_):
-        return likelihood_spline_skew(cube, x, y, x_knots, shift=2)
-
-    parameters = [r"$\alpha$", r"$\sigma$"] + [f"$k_{i}$" for i in range(len(x_knots))]
-    sampler = ultranest.ReactiveNestedSampler(
-        parameters, likelihood, prior,
-        vectorized=True, ndraw_min=nlive//2
-        # resume="overwrite", log_dir=f"log/{prefix}_"
-    )
-    results = sampler.run(
-        min_num_live_points=nlive,
-        dlogz=dlogz,
-        viz_callback=False
-    )
-    bf = results["maximum_likelihood"]["point"]
-    evidence = results["logz"]
-
-    if verbose:
-        cornerplot(sampler.results)
-        plt.show()
-
-    return evidence, bf
 
 
 def get_y_spline(x, *params):
